e1020da84e789289a7f736bb3a56b0532deb0e21
vegodiego <53877309+vegodiego@users.noreply.github.com>
2019-10-29 15:56:35 -0500

    Update tasks.rb

--- tasks.rb
+++ tasks.rb
@@ -2,33 +2,33 @@ require "sinatra"
 require "make_todo"
 
 get "/" do 
-	@database=Tarea.all
-	erb :index
+    @database=Tarea.all
+    erb :index
 end
 
 post "/new" do
-	Tarea.create(params[:newTask])
-	redirect '/'
+    Tarea.create(params[:newTask])
+    redirect '/'
 end
 
 get "/:id/delete" do
-	@task=Tarea.find(params[:id])
-	erb :formDelete
+    @task=Tarea.find(params[:id])
+    erb :formDelete
 end
 
 get "/:id/complete" do
-	@task=Tarea.find(params[:id])
-	erb :formComplete
+    @task=Tarea.find(params[:id])
+    erb :formComplete
 end
 
 put "/:id/put" do
-	Tarea.update(params[:id])
-	redirect '/'
+    Tarea.update(params[:id])
+    redirect '/'
 end
 
 delete "/:id/deleteTask" do
-	Tarea.destroy(params[:id])
-	redirect '/'
+    Tarea.destroy(params[:id])
+    redirect '/'
 end
 
 
